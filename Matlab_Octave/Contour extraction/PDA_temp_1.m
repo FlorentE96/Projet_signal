ab7c7596fca30dadddf6fd6b7412c0ea445ae94d
FlorentE96 <flo.ecochard@gmail.com>
2017-03-01 15:58:48 +0100

    Finished contour extraction
    changed corr to biased, changed max detection routine. 60ms win for
    better perf

--- Matlab_Octave/Contour extraction/PDA_temp_1.m
+++ Matlab_Octave/Contour extraction/PDA_temp_1.m
@@ -6,7 +6,7 @@ clear; clc; close all;
 Fs=44100;
 duration = 1; % in seconds
 t=linspace(0,duration,duration*Fs); % time vector
-sine_freq1 = 1250; %input('Veuillez entrer la fréquence : '); % in hertz;
+sine_freq1 = 1250; %input('Veuillez entrer la frequence : '); % in hertz;
 sine_freq2 = 542;
 sample1 = sin(sine_freq1*2*pi*t);
 sample2 = sin(sine_freq2*2*pi*t);
@@ -17,7 +17,7 @@ sample_trim = sample1;%(1:100000); % first 3 seconds of sound (skip silence at b
 win_sz = floor(Fs * 0.01);
 prev=0;
 figure(1),
-plot(sample_trim, 'markerstyle', 'o');
+plot(sample_trim);
 grid on;
 
 %% ############ Methode 1 : zero-crossing ##############
@@ -31,8 +31,8 @@ for i = 1:win_sz:length(sample_trim)-win_sz
     % Frequency measurement
     %j/2
     freq(floor(j/2)) = 1/(zero_crossing_times(j+2)-zero_crossing_times(j));
-  endfor;
-endfor;
+  end;
+end;
 %freq=freq./2; % 2 zero-crossings per period
 figure(1),
 plot(freq)
